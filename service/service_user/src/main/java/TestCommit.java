74a75b2b246d254c2925c08c35263375bf46b886
rhythm <601268901@qq.com>
2020-09-19 11:43:43 +0800

    hello world

--- service/service_user/src/main/java/TestCommit.java
+++ service/service_user/src/main/java/TestCommit.java
@@ -1,3 +1,5 @@
+import javax.lang.model.SourceVersion;
+
 /**
  * @author : rhythm
  * @date : 2020/9/19 11:01
@@ -7,5 +9,6 @@ public class TestCommit {
         System.out.println("dasdsa");
         System.out.println("不打印args");
         System.out.println("args = " + args);
+        System.out.println("nihaoya");
     }
 }
