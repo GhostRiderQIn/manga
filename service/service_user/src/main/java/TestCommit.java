b082eb25140afb984c11cb8aa2e3035888fc546d
qinda <1300317478@qq.com>
2020-09-20 10:15:21 +0800

    test

--- service/service_user/src/main/java/TestCommit.java
+++ service/service_user/src/main/java/TestCommit.java
@@ -2,8 +2,10 @@
  * @author : rhythm
  * @date : 2020/9/19 11:01
  */
+
 public class TestCommit {
     public static void main(String[] args) {
+
         System.out.println("dasdsa");
         System.out.println("不打印args");
         System.out.println("d");
